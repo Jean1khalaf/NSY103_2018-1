8469748d6bd056b4d56817aa41eb17bd2818a9ba
Roy Geagea <roygeagea@Roys-MacBook-Air.local>
2017-10-29 17:41:04 +0200

    partie 1 + partie 2 a

---
+++ tpc/listing2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Entrez un caractère : " );
+    c = getchar();
+    printf("Voici le caractère entré : %c\n", c);
+    return 0;
+}
---
+++ tpc/listing22.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Entrez un caractère : " );
+    c = getchar();
+    printf("Voici le caractère entré : %d\n", c);
+    return 0;
+}
+
